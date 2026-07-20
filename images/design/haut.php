cb7efb7e664d9d83783c9f715987207d41fbc09e
Um@1r111 <shaikhumair297@gmail.com>
2026-07-20 14:51:38 +0500

    updated .gitignore for images and inc/config.php

---
+++ images/design/haut.php
@@ -0,0 +1,76 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<script type="text/javascript">
+  window.___gcfg = {lang: 'fr'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+<table id="blocheader" style="vertical-align:top;">
+<tbody>
+
+<tr>
+
+<td style="text-align:left;">
+<div style="margin-left:10px; line-height:219%;">
+
+<div style="padding-top:6px;" class="fb-like" data-href="http://www.al-hamdoulillah.com" data-send="false" data-layout="button_count" data-width="150" data-show-faces="true" data-action="recommend" data-font="verdana"></div> 
+
+
+<a href="https://twitter.com/alhamdoulillah" class="twitter-follow-button" data-width="250px" data-show-count="true" data-lang="fr" data-show-screen-name="false">Suivre @alhamdoulillah</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+<form method="post" action="http://www.servage.net/apps/maillist/">
+ <input name="maillist" value="24989" size="20" type="hidden">
+ <input name="redirect" value="http://www.al-hamdoulillah.com" type="hidden">
+ <input name="function" value="subscribe" size="20" type="hidden">
+  <div align="left">
+  <table>
+    <tbody>
+      <tr>
+        <td><input name="email" value="Le Mail du Rappel" onblur="if(this.value == '') { this.value= 'Votre email ici'; }" onclick="this.value = '';" size="21"><input value="Bismillah" name="subscribe" type="submit"></font></td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+</form>
+
+
+
+</div>
+
+</td>
+
+<td style="text-align:center; width:100%;">
+<div style="font-size: 10pt; font-family: verdana; margin-right: 24px; padding: 9px; height: 69px; border: 2px dashed black; text-align:left;">
+<div style="margin-top:3px; margin-left:9px;">Le prophète <span id="saws">(sallallahou 'alayhi wa sallam)</span> a dit :  « <span id="hadiths">Celui qui appelle au bien aura la récompense de celui qui le suivra, sans que leurs récompenses (respectives) soient pour autant diminuées. </span> » <span id="rapporteurs">[rapporté par Mouslim]</span></div>
+</div>
+</td>
+
+<td style="text-align:right;">
+<div class="fb-like-box" style="height:60px;" data-href="http://www.facebook.com/pages/Al-Hamdoulillah/210617682369189" data-width="250" data-show-faces="false" data-stream="false" data-header="false"></div>
+
+<a style="padding-top:7px;" rel="nofollow" href="https://twitter.com/share" class="twitter-share-button" data-width="109px" data-url="www.al-hamdoulillah.com" data-text="Nouveau site sur l'islam" data-lang="fr">Tweeter</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+<a href="https://plus.google.com/117622762138030441772" style="text-decoration:none;"><img src="https://ssl.gstatic.com/images/icons/gplus-32.png" alt="" style="border:0;width:32px;height:32px;"/></a>
+<g:plusone size="medium"></g:plusone>
+
+
+
+</td>
+
+</tr>
+
+
+</tbody>
+</table>
---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+define('URL_PLAY_STORE', 'https://play.google.com/store/apps/details?id=org.imperiuminnovations.alhamdoulillah');
+define('URL_APP_STORE',  'https://apps.apple.com/us/app/al-hamdoulillah-islamic-app/id6758715700');
+define('URL_LEARN_MORE', 'https://al-hamdoulillah.imperiuminnovations.org/');
+define('URL_LEARN_MORE_FEATURES', 'https://al-hamdoulillah.imperiuminnovations.org/features');
+define('IS_LOCAL', in_array($_SERVER['HTTP_HOST'], ['localhost', 'alhamdoulillah.local']));
